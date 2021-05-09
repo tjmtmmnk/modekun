29045a9441a0d2cae5c348f04337dcbc51607533
tjmtmmnk <tsujimoto027@gmail.com>
2021-05-09 16:09:39 +0900

    get comment from yt live chat

---
+++ src/chat.ts
@@ -0,0 +1,5 @@
+export interface Chat {
+    author?: string;
+    message: string;
+    element: HTMLElement;
+};
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,26 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+import { Chat } from "./chat";
+
+window.addEventListener("load", () => {
+  const modekun = () => {
+    console.log('hello modekun');
+    const chatSection = document.querySelector<HTMLIFrameElement>("iframe");
+    if (!chatSection || !chatSection.contentWindow) return;
+    const chatBlocks = chatSection.contentWindow.document.querySelectorAll<HTMLElement>(
+        ".yt-live-chat-text-message-renderer"
+    );
+    const chats: Chat[] = [];
+    chatBlocks.forEach((chatBlock) => {
+      const messageElement = chatBlock.querySelector<HTMLElement>("#message");
+      if (!messageElement) return;
+      const authorElement = chatBlock.querySelector<HTMLElement>("#author-name");
+      const author = authorElement?.innerText;
+      chats.push({
+        author: author,
+        message: messageElement.innerText,
+        element: chatBlock,
+      });
+    });
+    console.log(chats);
+  };
+  window.setInterval(modekun, 5000);
 });
