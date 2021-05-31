dbcf154cc7edf40112e63f128d2c14fe6c3994c5
tjmtmmnk <tsujimoto027@gmail.com>
2021-05-31 09:56:21 +0900

    support mildom

---
+++ src/source/mildom.ts
@@ -0,0 +1,48 @@
+import { ISource } from "./source";
+import { IChat } from "../chat";
+
+const chatSelector = {
+  chatBlock: "#message-list-container .text-message",
+};
+
+export class Mildom implements ISource {
+  extractChats(): IChat[] {
+    const chatBlocks = document.querySelectorAll<HTMLElement>(
+      chatSelector.chatBlock
+    );
+    const chats: IChat[] = [];
+    for (const chatBlock of chatBlocks) {
+      if (!chatBlock.textContent) continue;
+      let parsedChat: IMildomChat;
+      try {
+        parsedChat = parseChat(chatBlock.textContent);
+      } catch (e) {
+        continue;
+      }
+      chats.push({
+        key: parsedChat.author + parsedChat.message,
+        author: parsedChat.author,
+        message: parsedChat.message,
+        element: chatBlock,
+      });
+    }
+    return chats;
+  }
+}
+
+interface IMildomChat {
+  author: string;
+  message: string;
+}
+
+const chatRegex = /^(.+)\u00A0:\u00A0(.+)/;
+const parseChat = (chatBlock: string): IMildomChat => {
+  const chatMatch = chatBlock.match(chatRegex);
+  if (chatMatch?.length === 3) {
+    return {
+      author: chatMatch[1],
+      message: chatMatch[2],
+    };
+  }
+  throw "format error";
+};
