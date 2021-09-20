580ec336a3fde0e0a9dd7ccf980d44a0716412a7
tjmtmmnk <tsujimoto027@gmail.com>
2021-09-20 15:17:47 +0900

    receive param in useParam hooks

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,24 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PopupPage from "./components/popup";
 
-import { defaultParamsV2, IParameterV2, KEY_MODEKUN_PARAM } from "./config";
-import { get } from "./storage";
+import { defaultParamsV2, IParameterV2 } from "./config";
+import { receiveRequest, sendRequest } from "./message";
 
 export const useParams = (): IParameterV2 | undefined => {
   const [params, setParams] = React.useState<IParameterV2>();
   let isMounted = true;
   React.useEffect(() => {
-    get<string | undefined>(KEY_MODEKUN_PARAM).then((paramKey) => {
-      if (paramKey) {
-        get<IParameterV2 | undefined>(paramKey).then((params) => {
-          if (isMounted) {
-            setParams(params);
-          }
-        });
-      } else {
-        if (isMounted) {
-          setParams(defaultParamsV2);
-        }
+    sendRequest({
+      type: "GET_PARAM",
+    });
+    receiveRequest("RECEIVE_PARAM").then((res) => {
+      const param = res.data?.param as IParameterV2;
+      console.log("receive!!!!!!!");
+      console.log(param);
+      if (isMounted) {
+        setParams(param ?? defaultParamsV2);
       }
     });
     return () => {
