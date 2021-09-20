fa9ddd84d5e9ac42308af4b404c04db7f5443dd4
tjmtmmnk <tsujimoto027@gmail.com>
2021-09-20 15:52:06 +0900

    need break!!!

--- src/background.ts
+++ src/background.ts
@@ -9,16 +9,22 @@ chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener(async (req: Message) => {
     switch (req.type) {
       case "UPDATE_PARAM": {
+        console.log("update param!!");
         if (!req?.data?.param) return;
         const paramKey = await get<string | undefined>(KEY_MODEKUN_PARAM);
         paramKey && (await set(paramKey, req.data.param));
+        break;
       }
       case "UPDATE_PARAM_KEY": {
+        console.log("update param key!");
         if (!req?.data?.key) return;
         await set<string>(KEY_MODEKUN_PARAM, req.data.key);
+        break;
       }
       case "GET_PARAM": {
         const paramKey = await get<string | undefined>(KEY_MODEKUN_PARAM);
+        console.log("get param!");
+        console.log(paramKey);
         const param = paramKey
           ? (await get<IParameterV2 | undefined>(paramKey)) ?? defaultParamsV2
           : defaultParamsV2;
@@ -30,6 +36,7 @@ chrome.runtime.onConnect.addListener((port) => {
           },
         };
         port.postMessage(res);
+        break;
       }
     }
   });
