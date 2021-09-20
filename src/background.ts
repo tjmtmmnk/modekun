844f060639877fd93330d73f920f7dee833ea188
tjmtmmnk <tsujimoto027@gmail.com>
2021-09-20 11:14:15 +0900

    update parameter in background

---
+++ src/background.ts
@@ -0,0 +1,26 @@
+import { Message } from "./message";
+import { KEY_MODEKUN_PARAM } from "./config";
+import { get, set } from "./storage";
+
+chrome.runtime.onConnect.addListener((port) => {
+  console.assert(port.name === "modekun");
+  port.onMessage.addListener(async (req: Message) => {
+    switch (req.type) {
+      case "UPDATE_PARAM": {
+        if (!req?.data?.param) return;
+        const paramKey = await get<string | undefined>(KEY_MODEKUN_PARAM);
+        paramKey && (await set(paramKey, req.data.param));
+      }
+    }
+  });
+});
+// chrome.runtime.onMessage.addListener(async (req: Message) => {
+//   switch (req.type) {
+//     case "UPDATE_PARAM": {
+//       console.log("update!!!!!");
+//       if (!req?.data?.param) return;
+//       const paramKey = await get<string | undefined>(KEY_MODEKUN_PARAM);
+//       paramKey && (await set(paramKey, req.data.param));
+//     }
+//   }
+// });
