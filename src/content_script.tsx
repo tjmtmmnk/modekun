31f776245a1c5552089552470150767b14d1d38b
tjmtmmnk <tsujimoto027@gmail.com>
2021-09-20 15:17:20 +0900

    update param key by changing location

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -7,15 +7,14 @@ import {
 import {
   defaultParamsV2,
   DEFAULT_EXECUTION_INTERVAL_MS,
-  getParams,
   IParameterV2,
   keyStreamer,
-  KEY_MODEKUN_PARAM,
   OBSERVATION_INTERVAL_MS,
 } from "./config";
-import { get, set, setItem } from "./storage";
+import { get, set } from "./storage";
 import { selectSource } from "./source/source";
 import { IKuromojiWorker } from "./kuromoji";
+import { sendRequest } from "./message";
 
 let worker: Worker | null;
 let api: IKuromojiWorker | null;
@@ -39,16 +38,26 @@ window.addEventListener("load", async () => {
     // TODO: make compatible for parameter
     const source = selectSource(window.location.href);
     const paramKey = keyStreamer(source.name, source.extractStreamer());
-    await set<string>(KEY_MODEKUN_PARAM, paramKey);
-    const params = (await get<IParameterV2>(paramKey)) ?? defaultParamsV2;
-    await set<IParameterV2>(paramKey, params);
+
+    sendRequest({
+      type: "UPDATE_PARAM_KEY",
+      data: {
+        key: paramKey,
+      },
+    });
 
     worker = await createKuromojiWorker();
     api = await createKuromojiWorkerApi(worker, getDicPath());
 
     const modekun = async () => {
       window.clearTimeout(timerId);
-      if (!source) return;
+
+      const paramKey = keyStreamer(source.name, source.extractStreamer());
+      const params =
+        (await get<IParameterV2 | undefined>(paramKey)) ?? defaultParamsV2;
+
+      console.log(paramKey);
+      console.log(params);
 
       if (!api) {
         timerId = window.setTimeout(modekun, DEFAULT_EXECUTION_INTERVAL_MS);
@@ -59,13 +68,6 @@ window.addEventListener("load", async () => {
       if (chats.length < 1) {
         // NOTE: Don't terminate worker here.
         // Because an archive video may be able to open a chat section which was closed at first.
-        timerId = window.setTimeout(modekun, DEFAULT_EXECUTION_INTERVAL_MS);
-        return;
-      }
-
-      const params = await get<IParameterV2>(paramKey);
-      console.log(params);
-      if (!params) {
         timerId = window.setTimeout(modekun, DEFAULT_EXECUTION_INTERVAL_MS);
         return;
       }
@@ -90,6 +92,15 @@ let previousLocation = window.location.href;
 const observeLocation = async () => {
   const currentLocation = window.location.href;
   if (currentLocation !== previousLocation) {
+    const source = selectSource(currentLocation);
+    const paramKey = keyStreamer(source.name, source.extractStreamer());
+    sendRequest({
+      type: "UPDATE_PARAM_KEY",
+      data: {
+        key: paramKey,
+      },
+    });
+
     worker && terminateWorker(worker);
     // avoid memory leak, worker allocates a lot of memory
     worker = null;
